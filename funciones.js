ef1cf043f5a29fc2a3c95cc962a87779ef3907ab
GVLN <63563313+Gavilan-S@users.noreply.github.com>
2023-02-24 12:32:43 -0500

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,9 @@
+function accionUno()
+{
+	document.getElementById("salidaUno").innerHTML="keith haring, keith haring, keith haring, keith haring";
+}
+
+function accionDos()
+{
+	document.getElementById("salidaDos").innerHTML="Gustav Klimt, George Orwell";
+}
